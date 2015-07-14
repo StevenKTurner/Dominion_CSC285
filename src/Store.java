9d48319d74b502cdb8d8836b617a040700cfb105
zach2519 <zach2519@gmail.com>
2015-07-14 14:55:09 -0500

    Store Class
    Store Class

--- src/Store.java
+++ src/Store.java
@@ -1,21 +1,166 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.*;
 
 /**
- *
- * @author Steven
- */
-class Store {
+ * @author Zach
+ **/
 
-    boolean cardInStock(Card storeCard) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void removeCard(Card card) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+public class Store implements Card {
+    
+    //Main ArrayList to hold the 10 action card store stacks.
+    ArrayList<ArrayList<Card>> storeStacks = new ArrayList<>();
+    
+    //Ten action cards store stacks.
+    ArrayList<Card> cellarStack = new ArrayList<>(10);
+    ArrayList<Card> marketStack = new ArrayList<>(10);
+    ArrayList<Card> mineStack = new ArrayList<>(10);
+    ArrayList<Card> militiaStack = new ArrayList<>(10);
+    ArrayList<Card> woodcutterStack = new ArrayList<>(10);
+    ArrayList<Card> villageStack = new ArrayList<>(10);
+    ArrayList<Card> smithyStack = new ArrayList<>(10);
+    ArrayList<Card> workshopStack = new ArrayList<>(10);
+    ArrayList<Card> remodelStack = new ArrayList<>(10);
+    ArrayList<Card> moatStack = new ArrayList<>(10);
+    
+    //Victory point card store stacks.
+    ArrayList<Card> estateStack = new ArrayList<>(12);
+    ArrayList<Card> dutchyStack = new ArrayList<>(12);
+    ArrayList<Card> provinceStack = new ArrayList<>(12);
+    
+    //int variable to hold the number of empty store stacks.
+    int emptyStacks;
+       
+    public void createStacks () {
+        //Adds the ten store stacks ArrayLists to one master ArrayList
+        storeStacks.add(cellarStack);
+        storeStacks.add(marketStack);
+        storeStacks.add(mineStack);
+        storeStacks.add(militiaStack);
+        storeStacks.add(woodcutterStack);
+        storeStacks.add(villageStack);
+        storeStacks.add(smithyStack);
+        storeStacks.add(workshopStack);
+        storeStacks.add(remodelStack);
+        storeStacks.add(moatStack);
+              
+        //How will the ten action card stack ArrayLists be populated with ten 
+        //cards without having to use ten separate for loops? 
     }
     
+    public void removeCard (Card card) {
+        String name = card.getName();
+        
+        //Returns the name of the card passed as a parameter when removeCard 
+        //is called. The switch statement determines which ArrayList is going
+        //to have a card removed from it.
+        switch (name) {
+            case "cellar":
+                cellarStack.remove(cellarStack.size());
+                break;
+            case "market":
+                marketStack.remove(marketStack.size());
+                break;
+            case "mine":
+                mineStack.remove(mineStack.size());
+                break;
+            case "militia":
+                militiaStack.remove(militiaStack.size());
+                break;
+            case "woodcutter":
+                woodcutterStack.remove(woodcutterStack.size());
+                break;
+            case "village":
+                villageStack.remove(villageStack.size());
+                break;
+            case "smithy":
+                smithyStack.remove(smithyStack.size());
+                break;
+            case "workshop":
+                workshopStack.remove(workshopStack.size());
+                break;
+            case "remodel":
+                remodelStack.remove(remodelStack.size());
+                break;
+            case "moat":
+                moatStack.remove(moatStack.size());
+                break;
+            case "estate":
+                estateStack.remove(estateStack.size());
+                break;
+            case "dutchy":
+                dutchyStack.remove(dutchyStack.size());
+                break;
+            case "province":
+                provinceStack.remove(provinceStack.size());
+                break;
+            default:
+                System.out.println("This default statement should never be reached");
+        }
+    }
+    
+    public boolean countEmptyStacks () {
+        
+        //Loops through the 10 action card stacks via the storeStacks "master"
+        //ArrayList to count how many are empty.
+        for (ArrayList<Card> storeStack : storeStacks) {
+            if (storeStack.isEmpty()) {
+                emptyStacks++;
+            }
+        }
+        //Returns true if 3 or more stacks are empty, false otherwise.
+        return emptyStacks >= 3;
+    }
+    
+    public boolean cardInStock (Card card) {
+        String name = card.getName();
+        boolean inStock = true;
+        
+        //Similar to the switch statement in the removeCard method. Determines
+        //the name of the card passed as a parameter and sets the boolean variable
+        //inStock to true or false and then returns inStock.
+        switch (name) {
+            case "cellar":
+                inStock = cellarStack.isEmpty();
+                break;
+            case "market":
+                inStock = marketStack.isEmpty();
+                break;
+            case "mine":
+                inStock = mineStack.isEmpty();
+                break;
+            case "militia":
+                inStock = militiaStack.isEmpty();
+                break;
+            case "woodcutter":
+                inStock = woodcutterStack.isEmpty();
+                break;
+            case "village":
+                inStock = villageStack.isEmpty();
+                break;
+            case "smithy":
+                inStock = smithyStack.isEmpty();
+                break;
+            case "workshop":
+                inStock = workshopStack.isEmpty();
+                break;
+            case "remodel":
+                inStock = marketStack.isEmpty();
+                break;
+            case "moat":
+                inStock = moatStack.isEmpty();
+                break;
+            case "estate":
+                inStock = estateStack.isEmpty();
+                break;
+            case "dutchy":
+                inStock = dutchyStack.isEmpty();
+                break;
+            case "province":
+                inStock = provinceStack.isEmpty();
+                break;
+            default:
+                System.out.println("This default statement should never be reached");
+        }
+        
+        return inStock;
+    }
 }
