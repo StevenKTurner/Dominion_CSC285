02994c6cd6827f04c6bab2094ee1bf428fa3550a
Neal <neal.traweek@gmail.com>
2015-07-15 21:10:14 -0500

    Added
    added getAction and setAction

--- src/ActionStack.java
+++ src/ActionStack.java
@@ -9,5 +9,36 @@
  * @author n00000158
  */
 public interface ActionStack {
+
+        int value = 0;
+        Card type;
+	  Card action;
+
+	  int buyPoints = 0;
+
+        private ActionStack(int value, Card type) {
+                this.value = value;
+                this.type = type;
+        }
+
+        public int value() {
+                return this.value;
+        }
+
+        public Card getType() {
+                return this.type;
+        }
+
+        public String toString() {
+                return this.type.name();
+        }
+
+	  public Card getAction(){
+	  this.action = action;
+}
+	  public Card setAction(){
+	  		return this.action;
+}
+}
     
 }
